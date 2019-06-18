9a9ae258ac58f27edf2b0b425ddac0c4cebdd093
Sanaxen <bccatfalcon@gmail.com>
2019-06-18 23:01:31 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # cpp_torch
+## [libtorch](https://pytorch.org/get-started/locally/) atVisual Studio C++  
+
+Directory structure
+<img src="./images/image00.png"/>  
+
+## Requirements  
+visual studio 2015,2017  
+
+[libtorch](https://pytorch.org/get-started1locally/)
+**Please adapt the version of cuda to your environment**  
+<img src="./images/image01.png"/>  
---
+++ torch_cpp/torch_util/libtorch.hpp
@@ -0,0 +1,23 @@
+#ifndef libtorch_LIBS
+#define libtorch_LIBS
+
+#pragma comment(lib, "c10.lib")
+#pragma comment(lib, "c10_cuda.lib")
+#pragma comment(lib, "caffe2.lib")
+#pragma comment(lib, "caffe2_detectron_ops_gpu.lib")
+#pragma comment(lib, "caffe2_gpu.lib")
+#pragma comment(lib, "caffe2_module_test_dynamic.lib")
+#pragma comment(lib, "clog.lib")
+#pragma comment(lib, "cpuinfo.lib")
+#pragma comment(lib, "foxi_dummy.lib")
+#pragma comment(lib, "foxi_loader.lib")
+#pragma comment(lib, "libprotobuf-lite.lib")
+#pragma comment(lib, "libprotobuf.lib")
+#pragma comment(lib, "libprotoc.lib")
+#pragma comment(lib, "onnx.lib")
+#pragma comment(lib, "onnxifi_dummy.lib")
+#pragma comment(lib, "onnxifi_loader.lib")
+#pragma comment(lib, "onnx_proto.lib")
+#pragma comment(lib, "torch.lib")
+
+#endif
