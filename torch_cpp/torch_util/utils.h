df698b5546b5462a9ae5840ba5eed7ad6a6856d9
Sanaxen <bccatfalcon@gmail.com>
2019-07-02 03:16:17 +0900

    Update utils.h

--- torch_cpp/torch_util/utils.h
+++ torch_cpp/torch_util/utils.h
@@ -37,7 +37,7 @@ inline bool is_little_endian() {
 }
 
 template <typename T>
-size_t max_index(const T &vec) {
+inline size_t max_index(const T &vec) {
   auto begin_iterator = std::begin(vec);
   return std::max_element(begin_iterator, std::end(vec)) - begin_iterator;
 }
