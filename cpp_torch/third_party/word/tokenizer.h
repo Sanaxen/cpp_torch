d05ace7e3f78360456f800fb1346c580fd4d7f6b
Sanaxen <bccatfalcon@gmail.com>
2019-08-19 23:26:44 +0900

    word_embed utility

---
+++ cpp_torch/third_party/word/tokenizer.h
@@ -0,0 +1,70 @@
+/*
+ * tokenizer.h
+ *
+ */
+
+#ifndef TOKENIZER_H_
+#define TOKENIZER_H_
+
+#ifdef USE_MECAB
+#include <mecab.h>
+#endif
+#include <string>
+
+std::vector<std::string> split(const std::string &str, char sep)
+{
+    std::vector<std::string> v;        // 分割結果を格納するベクター
+    auto first = str.begin();              // テキストの最初を指すイテレータ
+    while( first != str.end() ) {         // テキストが残っている間ループ
+        auto last = first;                      // 分割文字列末尾へのイテレータ
+        while( last != str.end() && *last != sep )       // 末尾 or セパレータ文字まで進める
+            ++last;
+        v.push_back(std::string(first, last));       // 分割文字を出力
+        if( last != str.end() )
+            ++last;
+        first = last;          // 次の処理のためにイテレータを設定
+    }
+    return v;
+}
+
+#ifdef USE_MECAB
+class Tokenizer {
+private:
+    MeCab::Tagger *tagger;
+
+public:
+    Tokenizer(){
+        //tagger = MeCab::createTagger("-Owakati");
+        //tagger = MeCab::createTagger("-xunknown -d /usr/local/lib/mecab/dic/mecab-ipadic-neologd");
+        tagger = MeCab::createTagger("-d /usr/local/lib/mecab/dic/mecab-ipadic-neologd");
+    }
+    ~Tokenizer(){
+        delete tagger;
+    }
+
+    std::vector<std::string> parse(std::string input){
+
+
+        std::vector<std::string> result;
+
+        const MeCab::Node* node = tagger->parseToNode(input.c_str());
+
+        char buf[1024];
+
+        for (; node; node = node->next) {
+            string feature(node->feature);
+            //if (feature.find("名詞")==0 || feature.find("未知語")==0){
+                strcpy(buf,node->surface);
+                buf[node->length]='\0';
+                string surface(buf);
+                result.push_back(surface);
+            //}
+        }
+        return result;
+    }
+};
+
+#endif
+
+
+#endif /* TOKENIZER_H_ */
---
+++ cpp_torch/third_party/word/word_embed.h
@@ -0,0 +1,208 @@
+/*
+ * word_embed.h
+ *
+ */
+
+#ifndef WORD_EMBED_H_
+#define WORD_EMBED_H_
+
+#include <string>
+#include <sstream>
+#include "tokenizer.h"
+
+#define IDX2F(i,j,ld) ((((j))*(ld))+((i)))
+
+
+using namespace std;
+
+typedef pair<string, int> ass_arr;
+inline bool sort_less(const ass_arr& left,const ass_arr& right){
+    return left.second < right.second;
+}
+inline bool sort_greater(const ass_arr& left,const ass_arr& right){
+    return left.second > right.second;
+}
+
+class WordEmbed {
+
+#ifdef USE_MECAB
+    Tokenizer token;
+#endif
+
+    vector<vector<string>> sequences;
+    vector<vector<int>> sequences_ids;
+    map<string, int> idmap;
+    map<int, string> idmap_reverse;
+
+    map<string, int> words_count;
+
+
+
+    int vocab_size = 0;
+
+public:
+
+    const int UNK_ID = 0;
+    const int SOS_ID = 1;
+    const int EOS_ID = 2;
+    const int PAD_ID = 3;
+
+    WordEmbed(int vocab_size){
+
+        this->vocab_size = vocab_size;
+
+        idmap["<unk>"] = UNK_ID;
+        idmap_reverse[UNK_ID] = "<unk>";
+        idmap["<sos>"] = SOS_ID;
+        idmap_reverse[SOS_ID] = "<sos>";
+        idmap["<eos>"] = EOS_ID;
+        idmap_reverse[EOS_ID] = "<eos>";
+        idmap["<pad>"] = PAD_ID;
+        idmap_reverse[PAD_ID] = "<pad>";
+    }
+
+
+    int getWordCount(){
+        return words_count.size();
+    }
+
+    std::string replace( std::string String1, std::string String2, std::string String3 )
+    {
+        std::string::size_type  Pos( String1.find( String2 ) );
+
+        while( Pos != std::string::npos )
+        {
+            String1.replace( Pos, String2.length(), String3 );
+            Pos = String1.find( String2, Pos + String3.length() );
+        }
+
+        return String1;
+    }
+
+    std::vector<std::string> split(const std::string &str, char sep)
+    {
+        std::vector<std::string> v;
+        std::stringstream ss(str);
+        std::string buffer;
+        while( std::getline(ss, buffer, sep) ) {
+            v.push_back(buffer);
+        }
+        return v;
+    }
+
+
+	void addSentences(vector<string> seqs, bool tokenize, bool addEOS)
+	{
+        for (auto s : seqs){
+			add(s, tokenize, addEOS);
+		}
+
+        vector<pair<string, int> > pairs(words_count.size());
+
+        int i=0;
+        for (auto v : words_count){
+            pairs[i] = make_pair(v.first, v.second);
+            i++;
+        }
+
+        sort(pairs.begin(),pairs.end(),sort_greater);
+
+        int cnt = 0;
+        for(auto v : pairs){
+            string w = v.first;
+            if (idmap.count(w) == 0){
+                int id = idmap.size();
+                idmap[w] = id;
+                idmap_reverse[id] = w;
+            }
+            if (cnt == vocab_size) break;
+            cnt++;
+        }
+
+        for (int i=0; i<sequences.size(); i++){
+            vector<string> words = sequences[i];
+            vector<int> word_ids;
+            for (int j=0; j<words.size(); j++){
+                if (idmap.count(words[j]) == 0)word_ids.push_back(UNK_ID);
+                else word_ids.push_back(idmap[words[j]]);
+            }
+            sequences_ids.push_back(word_ids);
+
+        }
+
+    }
+
+	void add(string sentence, bool tokenize, bool addEOS)
+	{
+		if (sentence == "") return;
+
+
+        vector<string> words, words_final;
+#ifdef USE_MECAB
+		if (tokenize) words = token.parse(sentence);
+		else 
+		words = split(sentence, ' ');
+#else
+		words = split(sentence, ' ');
+#endif
+
+        if (addEOS) words.push_back("<eos>");
+
+        for (auto w : words){
+            if (w != "") words_final.push_back(w);
+        }
+
+        for (auto w : words_final) {
+            if (words_count.count(w) == 0) words_count[w] = 1;
+            else words_count[w] += 1;
+        }
+
+        sequences.push_back(words_final);
+    }
+
+
+    vector<vector<int>> getSequencesIds(){
+        return sequences_ids;
+    }
+
+    void padding(vector<int> &ids, int max_size){
+
+        int padding_nums = max_size - ids.size();
+        if (padding_nums > 0){
+            for (int i=0; i<padding_nums; i++){
+                ids.push_back(PAD_ID);
+            }
+        }
+    }
+
+    void paddingAll(int max_size){
+        for (int i=0; i<sequences_ids.size(); i++){
+
+            this->padding(sequences_ids[i], max_size);
+        }
+    }
+
+    void toOneHot(int v_size, float *data, int id, int col, bool ignore){
+
+        for(int i=0; i<v_size; i++){
+            if (i==id && !ignore) data[IDX2F(i, col, v_size)] = 1.;
+            else data[IDX2F(i, col, v_size)] = 0.;
+        }
+    }
+
+    vector<vector<string>> getSequences(){
+        return sequences;
+    }
+
+    string toWord(int id){
+        return idmap_reverse[id];
+    }
+    int toId(string w){
+        return idmap[w];
+    }
+
+};
+
+
+
+#endif /* WORD_EMBED_H_ */
